b67e35b1febd1cf5a3dbcb064e18cae579fa40e1
Alvin Ahb <alvin.arhab@gmail.com>
2024-06-08 15:49:55 +0200

    Initial models

--- supplyme/admin.py
+++ supplyme/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import (Category,
+                     Organization,
+                     Product,
+                     User)
 
+
+admin.site.register(Category)
+admin.site.register(Organization)
 admin.site.register(Product)
+admin.site.register(User)
--- supplyme/models.py
+++ supplyme/models.py
@@ -1,10 +1,52 @@
 from django.db import models
 
 
-class Product(models.Model):
+class DateFields:
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Organization(models.Model, DateFields):
+    CST = 'CUSTOMER'
+    PRV = 'PROVIDER'
+    TYPE_CHOICES = (
+        (CST, 'Customer'),
+        (PRV, 'Provider'),
+    )
+
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES, null=False)
     name = models.CharField(max_length=40, null=False)
     description = models.CharField(max_length=200)
-    price = models.FloatField(null=False)
+    address = models.CharField(max_length=40, null=False)  # To be encrypted
 
     def __str__(self):
         return self.name
+
+
+class User(models.Model, DateFields):
+    username = models.CharField(max_length=20, null=False)
+    password = models.CharField(max_length=40, null=False)  # To be encrypted
+    email  = models.CharField(max_length=20, null=False)  # To be encrypted
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40, null=False)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model, DateFields):
+    name = models.CharField(max_length=40, null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
+    description = models.CharField(max_length=200)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, null=False)
+    price = models.FloatField(null=False)
+
+    def __str__(self):
+        return " - ".join((self.name, self.provider))
